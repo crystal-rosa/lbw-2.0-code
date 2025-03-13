9b8a55fb1ef7e8785bf8e02fe2da7c446958f683
crystal rosa <s@iMac.attlocal.net>
2025-03-12 19:33:17 -0700

    Updated something

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryRed: "#A10000",
+        darkOrange: "#CC6123",
+        lightOrange: "#E67635",
+        goldAccent: "#8D6926",
+        cremeAccent: "#EDEADD",
+        blackAccent: "#181818",
+        greyBG: "#F5F5F5",
+      },
+      fontFamily: {
+        brandon: ["brandon-grotesque", "sans-serif"],
+        source: ["Source Sans 3", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 };
