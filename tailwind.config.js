ae129d6721267d7310b1b2ca64bd57f2b2c0d294
crystal rosa <crystalrosa@crystals-MacBook-Pro.local>
2025-03-23 13:04:31 -0700

    Cleaned up structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,22 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: ["./*.html"],
-//   theme: {
-//     extend: {
-//       colors: {
-//         primaryRed: "#A10000",
-//         darkOrange: "#CC6123",
-//         lightOrange: "#E67635",
-//         goldAccent: "#8D6926",
-//         cremeAccent: "#EDEADD",
-//         blackAccent: "#181818",
-//         greyBG: "#F5F5F5",
-//       },
-//       fontFamily: {
-//         brandon: ["brandon-grotesque", "sans-serif"],
-//         source: ["Source Sans 3", "sans-serif"]
-//       }
-//     },
-//   },
-//   plugins: [],
-// };
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primaryRed: "#A10000",
+        darkOrange: "#CC6123",
+        lightOrange: "#E67635",
+        goldAccent: "#8D6926",
+        cremeAccent: "#EDEADD",
+        blackAccent: "#181818",
+        greyBG: "#F5F5F5",
+      },
+      fontFamily: {
+        brandon: ["brandon-grotesque", "sans-serif"],
+        source: ["Source Sans 3", "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+};
